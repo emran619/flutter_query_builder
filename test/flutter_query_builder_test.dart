3fe4449b65518e8be4277020321a2652a3efb2c6
Emran <amranmaher0@gmail.com>
2023-10-13 17:26:53 -0700

    refactoring test file

--- test/flutter_query_builder_test.dart
+++ test/flutter_query_builder_test.dart
@@ -1,16 +1,3 @@
 import 'package:flutter_query_builder/flutter_query_builder.dart';
-import 'package:test/test.dart';
 
-void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
-  });
-}
+void main() {}
